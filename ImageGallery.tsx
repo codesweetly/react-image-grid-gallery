49c40a979383f3e381103490acb2c06a519f79b6
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-05-22 19:41:40 +0100

    feat(ImageGallery): Create ImageGallery component

---
+++ ImageGallery.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import FsLightbox from "fslightbox-react";
+import uniqid from "uniqid";
+
+export default function ImageGallery({ imgArray }) {
+  const [lightboxController, setLightboxController] = useState({
+    toggler: false,
+    slide: 1,
+  });
+
+  const imgElementArray = imgArray.map((item, index) => (
+    <figure className="image-figure" key={uniqid()}>
+      <img
+        alt={item.alt}
+        src={item.src}
+        className="codesweetly-img"
+        onClick={() => openLightboxOnSlide(index + 1)}
+      />
+      <figcaption>{item.caption}</figcaption>
+    </figure>
+  ));
+
+  function openLightboxOnSlide(number) {
+    setLightboxController({
+      toggler: !lightboxController.toggler,
+      slide: number,
+    });
+  }
+
+  return (
+    <div className="codesweetly-imgs-container">
+      {imgElementArray}
+      <FsLightbox
+        toggler={lightboxController.toggler}
+        slide={lightboxController.slide}
+        sources={imgArray.map((item) => item.src)}
+      />
+    </div>
+  );
+}
