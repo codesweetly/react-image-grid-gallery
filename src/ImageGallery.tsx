4529b1eba963c3827db876422e9756a75162e3c7
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-12-21 19:50:18 +0100

    refactor: Move css rulesets into imagegallerystyles file

--- src/ImageGallery.tsx
+++ src/ImageGallery.tsx
@@ -1,45 +1,8 @@
 import React, { useState } from "react";
 import { ImageGalleryPropsType } from "./ImageGallery.types";
+import { ImageGalleryStyles } from "./ImageGalleryStyles";
 import FsLightbox from "fslightbox-react";
 import uniqid from "uniqid";
-
-class ImageGalleryStyles {
-  galleryContainerStyle: React.CSSProperties;
-  imageContainerStyle: React.CSSProperties;
-  imageStyle: React.CSSProperties;
-  imageCaptionStyle: React.CSSProperties;
-
-  constructor(gapSize?: number, columnWidth?: number) {
-    this.galleryContainerStyle = {
-      columnWidth: `${columnWidth}px`,
-      columnGap: `${gapSize}px`,
-    };
-    this.imageContainerStyle = {
-      marginBottom: `${gapSize}px`,
-      margin: 0,
-      position: "relative",
-    };
-    this.imageStyle = {
-      display: "block",
-      width: "100%",
-      cursor: "pointer",
-    };
-    this.imageCaptionStyle = {
-      opacity: 0,
-      position: "absolute",
-      bottom: 0,
-      zIndex: "1000",
-      width: "100%",
-      backgroundColor: "rgba(0, 0, 0, 0.5)",
-      background:
-        "linear-gradient(180deg, rgba(25, 27, 38, 0), rgba(25, 27, 38, 1))",
-      padding: "16px",
-      fontSize: "0.85rem",
-      textAlign: "center",
-      color: "#fff",
-    };
-  }
-}
 
 export function ImageGallery({
   imagesInfoArray,
@@ -50,7 +13,6 @@ export function ImageGallery({
     toggler: false,
     slide: 1,
   });
-
   const galleryContainerStyle = new ImageGalleryStyles(gapSize, columnWidth)
     .galleryContainerStyle;
   const imageContainerStyle = new ImageGalleryStyles(gapSize)
---
+++ src/ImageGalleryStyles.ts
@@ -0,0 +1,37 @@
+export class ImageGalleryStyles {
+  galleryContainerStyle: React.CSSProperties;
+  imageContainerStyle: React.CSSProperties;
+  imageStyle: React.CSSProperties;
+  imageCaptionStyle: React.CSSProperties;
+
+  constructor(gapSize?: number, columnWidth?: number) {
+    this.galleryContainerStyle = {
+      columnWidth: `${columnWidth}px`,
+      columnGap: `${gapSize}px`,
+    };
+    this.imageContainerStyle = {
+      marginBottom: `${gapSize}px`,
+      margin: 0,
+      position: "relative",
+    };
+    this.imageStyle = {
+      display: "block",
+      width: "100%",
+      cursor: "pointer",
+    };
+    this.imageCaptionStyle = {
+      opacity: 0,
+      position: "absolute",
+      bottom: 0,
+      zIndex: "1000",
+      width: "100%",
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+      background:
+        "linear-gradient(180deg, rgba(25, 27, 38, 0), rgba(25, 27, 38, 1))",
+      padding: "16px",
+      fontSize: "0.85rem",
+      textAlign: "center",
+      color: "#fff",
+    };
+  }
+}
