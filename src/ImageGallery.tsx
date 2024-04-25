95f3e3114ee5a5192ef5f45dc9296158e90d7a7f
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-04-25 05:06:00 +0100

    feat: Allow the lightbox to be closed with the escape key

--- src/ImageGallery.tsx
+++ src/ImageGallery.tsx
@@ -102,7 +102,11 @@ export function ImageGallery({
               <path d="M5.5 0a.5.5 0 0 1 .5.5v4A1.5 1.5 0 0 1 4.5 6h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5m5 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 10 4.5v-4a.5.5 0 0 1 .5-.5M0 10.5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 6 11.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5m10 1a1.5 1.5 0 0 1 1.5-1.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0z" />
             )}
           </span>
-          <span style={modalNavBtnStyle} title="Close" onClick={closeLightbox}>
+          <span
+            style={modalNavBtnStyle}
+            title="Close"
+            onClick={() => setShowModal(false)}
+          >
             {SvgElement(
               <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
             )}
@@ -168,6 +172,7 @@ export function ImageGallery({
     e.currentTarget.style.outline = "none";
     e.key === "ArrowLeft" && changeSlide(-1);
     e.key === "ArrowRight" && changeSlide(1);
+    e.key === "Escape" && setShowModal(false);
   }
 
   function changeSlide(directionNumber: number) {
@@ -195,10 +200,6 @@ export function ImageGallery({
         {pathElement}
       </svg>
     );
-  }
-
-  function closeLightbox() {
-    setShowModal(false);
   }
 
   useEffect(() => {
