0614ed9a9cc0ce3b47ca54ccaa9d4a574d31444c
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-01-07 03:31:57 +0100

    feat!: Make an id field part of the required properties of the imagesInfoArray prop

--- src/ImageGallery.types.tsx
+++ src/ImageGallery.types.tsx
@@ -1,22 +1,16 @@
 export interface ImageGalleryStylesType {
-    galleryContainerStyle?: React.CSSProperties;
-    imageBtnStyle?: React.CSSProperties;
-    imageContainerStyle?: React.CSSProperties;
-    imageStyle?: React.CSSProperties;
-    imageCaptionStyle?: React.CSSProperties;
-    modalContainerStyle?: React.CSSProperties;
-    modalSlideNumberStyle?: React.CSSProperties;
-    modalToolbarStyle?: React.CSSProperties;
-    modalToolbarBtnStyle?: React.CSSProperties;
-    modalSlideShowSectionStyle?: React.CSSProperties;
-    modalImageStyle?: React.CSSProperties;
-    modalSlideBtnStyle?: React.CSSProperties;
-}
-
-    alt: string;
-    caption?: string;
-    src: string;
-    key?: string;
+  galleryContainerStyle?: React.CSSProperties;
+  imageBtnStyle?: React.CSSProperties;
+  imageContainerStyle?: React.CSSProperties;
+  imageStyle?: React.CSSProperties;
+  imageCaptionStyle?: React.CSSProperties;
+  modalContainerStyle?: React.CSSProperties;
+  modalSlideNumberStyle?: React.CSSProperties;
+  modalToolbarStyle?: React.CSSProperties;
+  modalToolbarBtnStyle?: React.CSSProperties;
+  modalSlideShowSectionStyle?: React.CSSProperties;
+  modalImageStyle?: React.CSSProperties;
+  modalSlideBtnStyle?: React.CSSProperties;
 }
 
 export interface ImageGalleryPropsType {
